e53ec15389096ceddc1251f2f40237650ec1ff34
Supaluk Jaroensuk <32286346+supalukbenz@users.noreply.github.com>
2022-08-03 00:13:37 +0700

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/benzsupaluk/Vistec/clone/demo/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/benzsupaluk/Vistec/clone/demo/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/benzsupaluk/Vistec/clone/demo/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/benzsupaluk/Vistec/clone/demo/src/css/custom.css'),
-];
