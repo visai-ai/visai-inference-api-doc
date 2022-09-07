4b3fada163ab23f0ff2aeeb316d8614bfc421739
benzsupaluk <supaluk.j@ku.th>
2022-09-07 15:20:55 +0700

    feat: add seo meta tag

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import clsx from "clsx";
 import styles from "./HomepageFeatures.module.css";
 import Link from "@docusaurus/Link";
@@ -6,13 +7,13 @@ import Link from "@docusaurus/Link";
 const FeatureList = [
   {
     title: "Machine Translation",
-    src: require("../../static/img/translation_logo.webp").default,
+    src: require("/img/translation_logo.webp").default,
     href: "/inference-api/introduction",
     description: <>Translate from one language to another.</>,
   },
   {
     title: "OCR General Document",
-    src: require("../../static/img/ocr_logo.webp").default,
+    src: require("/img/ocr_logo.webp").default,
     href: "/inference-api/introduction-1",
     description: (
       <>
@@ -22,7 +23,7 @@ const FeatureList = [
   },
   {
     title: "Sentiment Analysis",
-    src: require("../../static/img/sentiment_analysis_logo.webp").default,
+    src: require("/img/sentiment_analysis_logo.webp").default,
     href: "/inference-api/introduction-2",
     description: (
       <>Text contextual extraction to determine people's feelings.</>
@@ -30,13 +31,13 @@ const FeatureList = [
   },
   {
     title: "Speech to Text",
-    src: require("../../static/img/asr_logo.webp").default,
+    src: require("/img/asr_logo.webp").default,
     href: "/inference-api/introduction-3",
     description: <>Transcribing speech into text using deep neural networks.</>,
   },
   {
     title: "Word Tokenization",
-    src: require("../../static/img/word-tokenization_logo.webp").default,
+    src: require("/img/word-tokenization_logo.webp").default,
     href: "/inference-api/introduction-4",
     description: <>Splitting sentences of text into words.</>,
   },
@@ -64,7 +65,7 @@ export default function HomepageFeatures() {
       <div className="container">
         <div className="row">
           {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
+            <Feature key={idx} {...props}></Feature>
           ))}
         </div>
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import clsx from "clsx";
 import Layout from "@theme/Layout";
+import Head from "@docusaurus/Head";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
@@ -29,10 +30,20 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Introduction ${siteConfig.title}`}
-      description="VISAI API Documentation <head />"
-    >
+    <Layout title={siteConfig.title} description="VISAI API Documentation">
+      <Head>
+        <meta
+          property="og:image"
+          content="https://visai-ai.github.io/visai-inference-api-doc/assets/images/visai_logo_bg-187adad4c0115713b82b9d5b5bec16dc.png"
+        />
+        <meta property="og:description" content={siteConfig.tagline} />
+        <meta property="twitter:title" content={siteConfig.title} />
+        <meta name="twitter:card" content="summary" />
+        <meta
+          property="og:image"
+          content="https://visai-ai.github.io/visai-inference-api-doc/assets/images/visai_logo_bg-187adad4c0115713b82b9d5b5bec16dc.png"
+        />
+      </Head>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
